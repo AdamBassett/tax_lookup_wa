40ab4285f7bc7d87d56685b3ab429a9c65b0203f
Adam Bassett <adam@bassettandbrush.com>
2017-06-02 12:30:51 -0700

    Adds the getTax function to the class TaxLookup.

--- spec/TaxLookupSpec.php
+++ spec/TaxLookupSpec.php
@@ -8,8 +8,34 @@ use Prophecy\Argument;
 
 class TaxLookupSpec extends ObjectBehavior
 {
-    function it_is_initializable()
-    {
+    function it_is_initializable(){
         $this->shouldHaveType(TaxLookup::class);
+    }
+    function it_returns_tax_rate_for_seattle_address(){
+        $this->getTax('1801 E Spruce St','Seattle','98122')->shouldReturn(0.101);
+    }
+    function it_returns_tax_rate_for_bothell_address(){
+        $this->getTax('19401 Holly Hills Dr NE','Bothell','98011')->shouldReturn(0.100);
+    }
+    function it_returns_tax_rate_for_colville_address(){
+        $this->getTax('1150 S Main St','Colville','99114')->shouldReturn(0.076);
+    }
+    function it_returns_tax_rate_for_spokane_address(){
+        $this->getTax('304 W Shannon Ave','Spokane','99205')->shouldReturn(0.088);
+    }
+    function it_returns_tax_rate_for_spokane_county_address(){
+        $this->getTax('17224 N Highland Ln','spokane valley','99005')->shouldReturn(0.081);
+    }
+    function it_returns_tax_rate_for_leavenworth_address(){
+        $this->getTax('236 Birch St','Leavenworth','98826')->shouldReturn(0.084);
+    }
+    function it_returns_tax_rate_for_kennewick_address(){
+        $this->getTax('809 S Huntington St','Kennewick','99336')->shouldReturn(0.086);
+    }
+    function it_returns_tax_rate_for_olympia_address(){
+        $this->getTax('810 Milroy St SW','olympia','98502')->shouldReturn(0.088);
+    }
+    function it_returns_tax_rate_for_omak_address(){
+        $this->getTax('228 south cedar','omak','98841')->shouldReturn(0.081);
     }
 }
--- src/TaxLookup.php
+++ src/TaxLookup.php
@@ -2,4 +2,24 @@
 
 class TaxLookup
 {
+    public function getTax(string $adress, string $city, string $zip): float {
+        $tax_url = 'http://dor.wa.gov/AddressRates.aspx?output=xml&addr='.urlencode($adress).
+                        '&city='.urlencode($city).
+                        '&zip='.$zip;
+        return (float) $this->getRate($tax_url);
+    }
+
+    private function getRate(string $callString): string {
+		$respons = simplexml_load_string($this->callAPI($callString));
+		return (string)$respons['rate'];
+    }
+
+    private function callAPI(string $url){
+        $ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$respons = curl_exec($ch);
+		curl_close($ch);
+        return $respons;
+    }
 }
